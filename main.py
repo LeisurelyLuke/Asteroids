ce40efdd87a34925fe8c84496e2c6e8b13fdc81e
LeisurelyLuke <l.korzanowski@gmail.com>
2026-01-10 15:06:28 +0100

    limit fps

--- main.py
+++ main.py
@@ -5,17 +5,27 @@ from logger import log_state
 
 def main():
     pygame.init()
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    clock = pygame.time.Clock()
+    dt = 0
+
+    
     print(f"Starting Asteroids with pygame version: {pygame.version.ver}")
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
-    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+
     while True:
         log_state()
+
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
+
         screen.fill("black")
+        clock.tick(60)
+        dt = clock.tick(60) / 1000  # Delta time in seconds.
         pygame.display.flip()
+
 
 if __name__ == "__main__":
     main()
